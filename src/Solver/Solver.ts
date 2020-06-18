54e4e89d1db96e31d544a90a0e6d98394ad9067d
Tomáš Blatný <blatny.tomas@seznam.cz>
2020-06-18 06:04:02 +0200

    Fixed API url

--- src/Solver/Solver.ts
+++ src/Solver/Solver.ts
@@ -9,7 +9,7 @@ export class Solver
 	{
 		axios({
 			method: 'post',
-			url: 'http://api.satisfactorytools.local/v1/solver',
+			url: 'https://api.satisfactorytools.com/v1/solver',
 			data: productionRequest,
 		}).then((response) => {
 			if ('result' in response.data) {
