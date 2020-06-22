a80a9d2a63e7610bdad87f4ab70d22ac0fc42539
Heroicos_HM <houghtonawe@gmail.com>
2020-06-21 19:23:00 -0700

    Updated meta tags for most every source that could want it

--- www/index.php
+++ www/index.php
@@ -14,13 +14,25 @@
 	<meta name="msapplication-TileColor" content="#da532c">
 	<meta name="msapplication-config" content="/assets/images/icons/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
-	<!-- Meta tags for previewing in Discord, or other such apps. -->
+	<!-- Primary Meta Tags -->
 	<meta name="theme-color" content="#df691a">
 	<meta name="title" content="Satisfactory Tools" />
 	<meta name="type" content="website" />
 	<meta name="url" content="https://www.satisfactorytools.com/" />
 	<meta name="image" content="https://www.satisfactorytools.com/assets/images/icons/android-chrome-512x512.png" />
 	<meta name="description" content="A collection of powerful tools for planning and building the perfect base. Calculate your production or consumption, browse items, buildings, and schematics and share your builds with others!" />
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://www.satisfactorytools.com/">
+	<meta property="og:title" content="Satisfactory Tools">
+	<meta property="og:description" content="A collection of powerful tools for planning and building the perfect base. Calculate your production or consumption, browse items, buildings, and schematics and share your builds with others!">
+	<meta property="og:image" content="https://www.satisfactorytools.com/assets/images/icons/android-chrome-512x512.png">
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://www.satisfactorytools.com/">
+	<meta property="twitter:title" content="Satisfactory Tools">
+	<meta property="twitter:description" content="A collection of powerful tools for planning and building the perfect base. Calculate your production or consumption, browse items, buildings, and schematics and share your builds with others!">
+	<meta property="twitter:image" content="https://www.satisfactorytools.com/assets/images/icons/android-chrome-512x512.png">
 
 	<style>
 		[ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak {
